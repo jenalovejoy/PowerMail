91921abecc084a39389ab54387ee3936d66ccedf
DNL213 <dn.luu03@gmail.com>
2019-06-16 23:33:01 -0700

    Implement composing + sending emails.

--- Server/routes/smtp.js
+++ Server/routes/smtp.js
@@ -10,36 +10,48 @@ var router = express.Router();
 // TLS/STARTTLS Port: port 587 
 
 router.post('/smtp', function(req, res, next) {
-    const email = req.query.email + '@' + req.query.host + '.com';
-    const pw = req.query.auth;
-    res.send('respond');
+    // console.log(req.body.host);
+    // console.log(req.body.port);
+    // console.log(req.body.email);
+    // console.log(req.body.pw);
+    // console.log(req.body.from);
+    // console.log(req.body.recipient);
+    // console.log(req.body.subject);
+    // console.log(req.body.text);
+
+    let transporter = nodemailer.createTransport({
+        host: "smtp." + req.body.host + ".com",
+        port: req.body.port,
+        secure: false,
+        auth: {
+            user: req.body.email, 
+            pass:req.body.pw     
+        }
+    });
+    let emailMsg = {
+        from: req.body.from, 
+        to: req.body.recipient, 
+        subject: req.body.subject, 
+        text: req.body.text, 
+        html: req.body.html 
+    }
+
+
+    // let emailMsg = {
+    //     from: '"Dominic Luu" <dn.luu03@gmail.com>',
+    //     to: "d_luu17@yahoo.com",     // target recipient
+    //     subject: "Hello",            // subject line
+    //     text: "Hi hi hi",            // body in plain text
+    //     html: "<b>Hello World</b>"   // body in HTML
+    // }
+
+    transporter.sendMail(emailMsg);
+
+
+    res.send('Email Sent');
     res.end();
 });
 
 
-function sendEmail(auth, msg) {
-
-    // SMTP Auth
-    let transporter = nodemailer.createTransport({
-        host: "smtp." + req.query.host + ".com",
-        port: 587,
-        secure: false,
-        auth: {
-            user: email, 
-            pass: pw 
-        }
-    });
-
-    let emailMsg = {
-        from: '"Dominic Luu" <dn.luu03@gmail.com>',
-        to: "d_luu17@yahoo.com",     // target recipient
-        subject: "Hello",            // subject line
-        text: "Hi hi hi",            // body in plain text
-        html: "<b>Hello World</b>"   // body in HTML
-    }
-
-    // transporter.sendMail(emailMsg);
-
-}
 
 module.exports = router;
